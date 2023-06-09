f0d1c79adecbd8aa9fc9c4741d2d069898d3da36
Chris Esposito <admin@espotek.com>
2023-06-09 15:46:09 +1000

    Window Flags

--- Desktop_Interface/main.cpp
+++ Desktop_Interface/main.cpp
@@ -13,6 +13,7 @@ int main(int argc, char *argv[])
 #endif
 
     MainWindow w;
+    w.setWindowFlags(Qt::Window | Qt::FramelessWindowHint | Qt::WindowCloseButtonHint);
     w.showFullScreen();
 
     return a.exec();
